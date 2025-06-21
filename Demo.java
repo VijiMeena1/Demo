2eaf1a46355e084e97b08558a887c9159e529d11
viji2005 <vijimeena48@gmail.com>
2025-06-21 12:27:31 +0530

    Demo

---
+++ Demo.java
@@ -0,0 +1,21 @@
+class Demo
+{
+ public static void main(String[]args){
+        byte a=10;
+        short b=5;
+        int c=234;
+        long d =324567788;
+        float e=23.5f;
+        double f=23.343524252d;
+        Boolean g=true;
+        char h='$';
+  System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+System.out.println(h);
+        }
+}
