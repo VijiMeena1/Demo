abfdcd77a24bc0380a9378889087b7057e0e0574
viji2005 <vijimeena48@gmail.com>
2025-06-26 19:45:40 +0530

    AverageExe
    First program

---
+++ Average .java
@@ -0,0 +1,28 @@
+class Average
+{
+public static void main(String[] args){
+      byte A=10;
+      short B=5;
+      int C=25;
+  int ave=A+B+C;
+  int div=ave/3;
+System.out.print(div);
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
