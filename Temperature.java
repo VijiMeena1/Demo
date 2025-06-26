1cf900d24995464484b26d42be46cc00dc1150c0
viji2005 <vijimeena48@gmail.com>
2025-06-26 19:54:28 +0530

    Fahrenheit Exe
    Second program

---
+++ Temperature.java
@@ -0,0 +1,12 @@
+class Temperature 
+{
+public static void main(String[] args){
+     double Celsius=20;
+     double Fahrenheit= (Celsius * 1.8)+32;
+System.out.println(Celsius+"°C");
+System.out.println(Fahrenheit+"°F");
+}
+}
+
+
+ 
