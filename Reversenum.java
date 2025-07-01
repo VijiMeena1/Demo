ae51ab3ea2fe9c8a6d6fd24d67b36e15a5ddc537
viji2005 <vijimeena48@gmail.com>
2025-07-01 12:21:55 +0530

    Reversenum using loop

---
+++ Reversenum.java
@@ -0,0 +1,33 @@
+/*class Reversenum
+{
+public static void main(String[] args){
+for(int a=100;a>=1;a--){ //for loop Reverse order
+System.out.println(a);
+}
+}
+}*/
+
+
+/*class Reversenum
+{
+public static void main(String[] args){
+int a=100;
+while(a>=1){      //while loop 
+System.out.println(a);
+a--;
+}
+}
+}*/
+
+
+class Reversenum
+{
+public static void main(String[] args){
+int a=100;
+do{
+System.out.println(a);
+a--;
+}
+while(a>=1);
+}
+}
