a87b98511587b5e7506dd65b1cbfcbc8588151de
viji2005 <vijimeena48@gmail.com>
2025-07-01 16:24:34 +0530

    Divisible using all loop

---
+++ Divisible.java
@@ -0,0 +1,40 @@
+/*class Divisible
+{
+public static void main(String[] args){
+
+for(int a=1000;a>=100;a--){    //for loop
+if(a%8==0){
+System.out.println(a);
+}
+}
+}
+}*/
+
+
+/*class Divisible
+{
+public static void main(String[] args){
+int a=1000;
+while(a>=100){
+if(a % 8==0){
+System.out.println(a);
+}
+a--;
+}
+}}*/
+
+class Divisible
+{
+public static void main(String[] args){
+int a=1000;
+do{
+if(a%8==0){
+System.out.println(a);
+}
+a--;
+}
+while(a>=100);
+}
+}
+
+
