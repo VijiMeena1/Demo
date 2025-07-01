f6034520c3277cef5aa7b89fccda887457884447
viji2005 <vijimeena48@gmail.com>
2025-07-01 12:18:38 +0530

    Using all loops

---
+++ UsingLoop.java
@@ -0,0 +1,53 @@
+/*class UsingLoop
+{
+public static void main(String[] args){
+for(int a=1;a<=100;a++){
+System.out.println(a);
+}
+}
+}*/
+
+//USING FOR LOOP: 
+
+/*class UsingLoop
+{
+public static void main(String[] args){
+for(int a=1;a<=100;a++){
+if(a % 2 == 0){
+System.out.println(a);
+}
+}
+}
+}*/
+
+
+//USING WHILE LOOP:
+/*class UsingLoop
+{
+public static void main(String[] args){
+int a=1;
+while(a<=100){
+a++;
+if(a % 2==0){
+System.out.println(a);
+}
+}
+}
+}*/
+
+//USING DO WHILE LOOP:
+class UsingLoop
+{
+public static void main(String[] args){
+int a=1;
+do{
+a++;
+if(a%2==0){
+System.out.println(a);
+}
+}
+
+while(a<=100);
+}
+}
+
